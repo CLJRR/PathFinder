aa4e207bc9d1e787fa0ad9951ee163db79dec3dd
MrLegumes <124041395+MrLegumes@users.noreply.github.com>
2024-10-12 18:10:34 +0800

    jia rong says commit

--- api/employeeAPI.py
+++ api/employeeAPI.py
@@ -14,7 +14,8 @@ def get_employee(name):
     try:
         employee_document = dbCollection.document(name).get()
         if name.exists:
-            return jsonify(employee_document.todict()),200
+            employee_data = employee_document.to_dict()
+            return render_template('employeeDashboard.html',employee = employee_data)
         else:
             return jsonify({"error":"Employee not found"}), 404
     except Exception as e:
